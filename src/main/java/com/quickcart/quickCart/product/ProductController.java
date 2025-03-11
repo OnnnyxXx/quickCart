49d2a219a38302323c9ff6e0508eb869d93c48e2
vich1981 <git@github.com>
2025-03-11 23:12:28 +0300

    Fix: product 1-1 :change ProductController, ProductService and create ProductRepository.

--- src/main/java/com/quickcart/quickCart/product/ProductController.java
+++ src/main/java/com/quickcart/quickCart/product/ProductController.java
@@ -1,4 +1,43 @@
 package com.quickcart.quickCart.product;
 
+import com.quickcart.quickCart.order.Order;
+import com.quickcart.quickCart.order.OrderService;
+import com.quickcart.quickCart.user.User;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
 public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    @PostMapping("/stores/{storeId}/products")
+    public ResponseEntity<Product> createProduct(@Valid @RequestBody Product product, @PathVariable("storeId") Long storeId,/* CurrentUser*/ User user){
+        return productService.createProduct(product, storeId);
+    }
+
+    @GetMapping("/stores/{storeId}/products")
+    public List<Product> getProductsByStore(@PathVariable("storeId") Long storeId){
+        return productService.getProductsByStoreId(storeId);
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable("id") Long id){
+        return productService.getProductById(id);
+    }
+
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Product> updateProduct(@Valid @RequestBody Product product, @PathVariable("id") Long id){
+        ResponseEntity<Product> productResponse = productService.getProductById(id);
+        if(productResponse.getStatusCode() == HttpStatus.NOT_FOUND) return productResponse;
+        return productService.updateProductById(id, product);
+    }
+
 }
--- src/main/java/com/quickcart/quickCart/product/ProductService.java
+++ src/main/java/com/quickcart/quickCart/product/ProductService.java
@@ -1,4 +1,60 @@
 package com.quickcart.quickCart.product;
 
+import com.quickcart.quickCart.order.Order;
+import com.quickcart.quickCart.order.OrderService;
+import com.quickcart.quickCart.store.Store;
+import com.quickcart.quickCart.store.StoreService;
+import com.quickcart.quickCart.user.User;
+import com.quickcart.quickCart.user.UserService;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+@Service
 public class ProductService {
+    final private ProductRepository productRepository;
+    final private UserService userService;
+    final private OrderService orderService;
+    final private StoreService storeService;
+
+    public ProductService(ProductRepository productRepository, UserService userService, OrderService orderService, StoreService storeService) {
+        super();
+        this.productRepository = productRepository;
+        this.userService = userService;
+        this.orderService = orderService;
+        this.storeService = storeService;
+    }
+
+    public ResponseEntity<Product> createProduct(Product product, Long storeId) {
+        Store store = storeService.getStoreById(storeId);
+        product.setStore(store);
+        Product savedProduct = productRepository.save(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedProduct);
+    }
+
+    public List<Product> getProductsByStoreId(Long storeId) {
+        Store store = storeService.getStoreById(storeId);
+        return store.getProducts();
+    }
+
+    public ResponseEntity<Product> getProductById(Long id) {
+        return productRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build()
+                );
+    }
+
+    @Transactional
+    public ResponseEntity<Product> updateProductById(Long id, Product updateProduct) {
+        if (!productRepository.existsById(id)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        updateProduct.setId(id);
+        Product savedProduct = productRepository.save(updateProduct);
+        return ResponseEntity.ok(savedProduct);
+    }
 }
