80fbcbd943ab2681f64b541e15c9741b35a36fa0
OnnnyxXx <oooivanlau2@gmail.com>
2025-03-15 09:57:55 +0300

    Added: store endpoint + global exclusion + cors

---
+++ src/main/java/com/quickcart/quickCart/store/excaption/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.quickcart.quickCart.store.excaption;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    // TODO Add more exception
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String, String>> handleResponseStatusException(ResponseStatusException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", ex.getReason());
+        return ResponseEntity.status(ex.getStatusCode()).body(errorResponse);
+    }
+}
