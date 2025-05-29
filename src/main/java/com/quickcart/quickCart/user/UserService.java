d0c6a574e4ff8cde51a7a2e9fff5d6dc541c4235
OnnnyxXx <oooivanlau2@gmail.com>
2025-05-29 10:06:50 +0300

    fix: removed unnecessary imports one more

--- src/main/java/com/quickcart/quickCart/user/UserService.java
+++ src/main/java/com/quickcart/quickCart/user/UserService.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
